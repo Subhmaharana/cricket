060ad790281aeb70a7619cefaed0c3df9fbb4f88
Subhmaharana <subhransumaharana@gmail.com>
2014-11-11 17:49:10 +0530

    Create ScoreVO.java

---
+++ ScoreVO.java
@@ -0,0 +1,64 @@
+package com.wipro.web;
+
+public class ScoreVO {
+	String battingTeamName;
+	int battingTeamScore;
+	String batsmanName;
+	int batsmanScore;
+	String bowlingTeamName;
+	int bowlingTeamScore;
+	String bowlerName;
+	double overNumber;
+	public String getBattingTeamName() {
+		return battingTeamName;
+	}
+	public void setBattingTeamName(String battingTeamName) {
+		this.battingTeamName = battingTeamName;
+	}
+	public int getBattingTeamScore() {
+		return battingTeamScore;
+	}
+	public void setBattingTeamScore(int battingTeamScore) {
+		this.battingTeamScore = battingTeamScore;
+	}
+	public String getBatsmanName() {
+		return batsmanName;
+	}
+	public void setBatsmanName(String batsmanName) {
+		this.batsmanName = batsmanName;
+	}
+	public int getBatsmanScore() {
+		return batsmanScore;
+	}
+	public void setBatsmanScore(int batsmanScore) {
+		this.batsmanScore = batsmanScore;
+	}
+	public String getBowlingTeamName() {
+		return bowlingTeamName;
+	}
+	public void setBowlingTeamName(String bowlingTeamName) {
+		this.bowlingTeamName = bowlingTeamName;
+	}
+	public int getBowlingTeamScore() {
+		return bowlingTeamScore;
+	}
+	public void setBowlingTeamScore(int bowlingTeamScore) {
+		this.bowlingTeamScore = bowlingTeamScore;
+	}
+	public String getBowlerName() {
+		return bowlerName;
+	}
+	public void setBowlerName(String bowlerName) {
+		this.bowlerName = bowlerName;
+	}
+	public double getOverNumber() {
+		return overNumber;
+	}
+	public void setOverNumber(double overNumber) {
+		this.overNumber = overNumber;
+	}
+	
+	
+	
+
+}
